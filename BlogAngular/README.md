1481b1da70f028b4d3e7f4cb6e5e835265b69820
Daniela <102395421+FDaniela@users.noreply.github.com>
2023-05-21 14:33:26 -0300

    upload BlogAngular

---
+++ BlogAngular/README.md
@@ -0,0 +1,9 @@
+# BlogAngular
+
+Este é um projeto gerado com [Angular CLI](https://github.com/angular/angular-cli) version 15.2.6 para a criação de um Blog. O projeto inclui algumas noticias retiradas do [blog.angular.io](https://blog.angular.io/), com páginas dinâmicas que se a cada componente noticiado.
+
+
+### Imagem do Projeto:
+
+
+
---
+++ BlogAngular/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Angular v16 is here!",
+    "description": "Six months ago, we reached a significant milestone in Angular’s simplicity and developer experience by graduating the standalone APIs from developer preview. Today, we’re thrilled to share that we’re continuing the Angular Momentum with the biggest release since the initial rollout of Angular; making large leaps in reactivity, server-side rendering, and tooling. All this comes with dozens of quality-of-life improvements across feature requests, with over 2,500 combined thumbs up on GitHub!",
+    "photoCover":"https://miro.medium.com/v2/resize:fit:1100/format:webp/1*CNy2EclZxC1hmi_govb_7w.png"
+  },
+  {
+    "id":"2",
+    "title": "Page Under Construction!",
+    "description": "Until the end of times...",
+    "photoCover":"http://www.f1.net.au/wp-content/uploads/2014/04/under_construction.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Page Under Construction!",
+    "description": "Until the end of times...",
+    "photoCover":"http://www.f1.net.au/wp-content/uploads/2014/04/under_construction.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Page Under Construction!",
+    "description": "Until the end of times...",
+    "photoCover":"http://www.f1.net.au/wp-content/uploads/2014/04/under_construction.jpg"
+  }
+]
